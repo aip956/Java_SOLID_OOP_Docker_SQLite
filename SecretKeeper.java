5db1702d6d8ecf27166efb6ef8144214c43c7bc5
Anthea Ip <antheaip964@gmail.com>
2024-04-14 19:54:12 -0400

    Saving

--- SecretKeeper.java
+++ SecretKeeper.java
@@ -1,16 +1,23 @@
-import java.util.Random;
+// import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SecretKeeper extends Player {
     private Game game;
     public String secretCode;
     public int maxAttempts = 5;
     public int attemptsLeft = maxAttempts;
+    private boolean secretGenerated; // new var for API
 
     // SecretKeeper will determine if there is a secret and/or max attempts
     // in the CLI args. If not, it will create a secret and/or default
     // the max attempts
     public SecretKeeper(Game game, String[] args) {
         this.game = game;
+        this.secretGenerated = false; // new var for API
         if (hasSecretInArgs(args)) {
             secretCode = extractSecretFromArgs(args);
         } else {
@@ -57,18 +64,42 @@ public class SecretKeeper extends Player {
     }
     // generate a random secret if one was not included in args
     private String generateRandomSecret() {
-        Random num = new Random();
-        StringBuilder secretBuilder = new StringBuilder();
-        while (secretBuilder.length() < 4) {
-            int int_random = num.nextInt(9);
-            if (secretBuilder.indexOf(String.valueOf(int_random)) == -1) {
-                secretBuilder.append(String.valueOf(int_random));
-            }        
+        try {
+            URL url = new URL("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
         }
-        secretCode = secretBuilder.toString();
-        // System.out.println(secret);
-        return secretCode;
+
+        secretCode = response.toString().trim();
+        secretGenerated = true;
+    } catch (IOException e) {
+        e.printStackTrace(); // Handle error
     }
+}
+
+
+
+
+    //     Random num = new Random();
+    //     StringBuilder secretBuilder = new StringBuilder();
+    //     while (secretBuilder.length() < 4) {
+    //         int int_random = num.nextInt(9);
+    //         if (secretBuilder.indexOf(String.valueOf(int_random)) == -1) {
+    //             secretBuilder.append(String.valueOf(int_random));
+    //         }        
+    //     }
+    //     secretCode = secretBuilder.toString();
+    //     System.out.println("Secret: " + secretCode);
+    //     return secretCode;
+    // }
     // extract max attempts from args
     private int extractMaxAttemptsFromArgs(String[] args) {
         for (int i = 0; i < args.length; i++) {
